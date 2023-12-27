7c37610b65325a8e7d0ecf8584614094d812ffa9
MiliAxe <miliaxe0@gmail.com>
2023-12-28 01:53:24 +0330

    Update measure_performance_se.py to calculate cosine similarities

--- measure_performance_se.py
+++ measure_performance_se.py
@@ -3,14 +3,16 @@ import time
 
 from modules.bcolors import bcolors
 from modules.searchengine import SearchEngine
+from modules.dataprocessor import SentencePosition
+
 
 # testing the speed
-file_count = 5000
+file_count = 50000
 time_limit = 100
-calculate_occurrences = 0
+find_cosine_similarities = 0
 try:
     file_count = int(sys.argv[1])
-    calculate_occurrences = int(sys.argv[2])
+    find_cosine_similarities = int(sys.argv[2])
     time_limit = float(sys.argv[3])
 except IndexError:
     pass
@@ -22,12 +24,30 @@ for i in range(file_count):
 se = SearchEngine(paths)
 
 se.calculate_tf_idf_all_docs()
+
+if find_cosine_similarities:
+    query = "microsoft rba"
+
+    relevant_docs = sorted(se.cosine_similarities_docs(query), key=lambda x: x[2], reverse=True)
+    print(relevant_docs[0])
+    relevant_sentences = sorted(se.cosine_similarities_doc_sentences(query, relevant_docs[0][0]),
+                                key=lambda x: x[1], reverse=True)
+    print(relevant_sentences[0])
+    print(
+        f"relevant sentence: {se.dp.sentence_at(SentencePosition(relevant_docs[0][0], relevant_sentences[0][0]))}")
+
+    highest_tf_word = se.get_word_with_highest_tf_in_doc(relevant_docs[0][0])
+    highest_idf_word = se.get_top_five_idf_words_in_doc(relevant_docs[0][0])
+    print(f"highest tf word: {highest_tf_word}")
+
 end_time = time.time()
 exec_time = end_time - start_time
+
 print(f"Adding {bcolors.BLUE}{file_count}{bcolors.ENDC} files took {bcolors.BLUE}{exec_time:.3f}{bcolors.ENDC} seconds",
       end=" ")
-if calculate_occurrences:
-    print("(Occurrences were calculated too)")
+
+if find_cosine_similarities:
+    print("(Cosine similarities were calculated too)")
 else:
     print()
 try:

8aa3f4b76ccb7be513a2f838983cd1cb056b26f1
Milad Zarei <milicrat@outlook.com>
2023-12-27 19:25:16 +0330

    Refactor the code (Style cleanup)

--- modules/searchengine.py
+++ modules/searchengine.py
@@ -1,10 +1,11 @@
-import math
 import heapq
-import numpy.typing as npt
+import math
 from typing import Dict, List
-import numpy as np
 
-from .dataprocessor import DataProcessor, SentencePosition, tokenize
+import numpy as np
+import numpy.typing as npt
+
+from .dataprocessor import DataProcessor, tokenize
 
 
 class SearchEngine:
@@ -17,43 +18,47 @@ class SearchEngine:
         self.dp.paths = paths
         self.dp.generate()
 
-
-
-    # theres still room for performance improvement
+    # there's still room for performance improvement
     def tf_word_in_all_docs(self, word):
-        return self.dp.docs_occurances_list(word)/self.dp.word_count_in_each_doc
+        return self.dp.docs_occurances_list(word) / self.dp.word_count_in_each_doc
 
-    def tf_word_in_one_sentence(self, word, sentence_str):
+    @staticmethod
+    def tf_word_in_one_sentence(word, sentence_str):
         words = tokenize(sentence_str)
         words_count = len(words)
 
         return words.count(word) / words_count
-        
+
     def tf_word_in_doc_sentences(self, word, doc_index):
         word = word.lower()
         sentences = self.dp.document_sentences(doc_index)
         return np.array([self.tf_word_in_one_sentence(word, sentence) for sentence in sentences], dtype=float)
-    
+
     def idf_word_in_all_docs(self, word):
-        return math.log(len(self.dp.paths)/(len(self.dp.occur_dict[word])+1))
-    
+        return math.log(len(self.dp.paths) / (len(self.dp.occur_dict[word]) + 1))
+
     def idf_word_in_doc(self, word, doc_index):
-        return math.log(len(self.dp.document_sentences(doc_index))/(self.dp.count_sentences_with_word_in_document(word, doc_index)+1))
+        return math.log(len(self.dp.document_sentences(doc_index)) / (
+                    self.dp.count_sentences_with_word_in_document(word, doc_index) + 1))
 
     def calculate_tf_idf_all_docs(self):
         self.tf_idf_dict = {word: self.tf_word_in_all_docs(
-            word)*self.idf_word_in_all_docs(word) for word in self.dp.occur_dict}
-        
+            word) * self.idf_word_in_all_docs(word) for word in self.dp.occur_dict}
+
     def calculate_tf_idf_doc(self, doc_index):
-        self.tf_idf_sentences_dict = {word.lower(): self.tf_word_in_doc_sentences(word.lower(), doc_index)*self.idf_word_in_doc(word.lower(), doc_index) for word in self.dp.document_words(doc_index)}
+        self.tf_idf_sentences_dict = {
+            word.lower(): self.tf_word_in_doc_sentences(word.lower(), doc_index) * self.idf_word_in_doc(word.lower(),
+                                                                                                        doc_index) for
+            word in self.dp.document_words(doc_index)}
 
     def cosine_similarity_of_doc(self, query, doc_index):
         closest_query = [self.dp.get_closest_word_all_docs(word) for word in tokenize(query)]
-        tf_idf_query = {word:self.tf_word_in_one_sentence(word, query)*self.idf_word_in_all_docs(word) for word in closest_query}
+        tf_idf_query = {word: self.tf_word_in_one_sentence(word, query) * self.idf_word_in_all_docs(word) for word in
+                        closest_query}
 
         doc_words = self.dp.document_words(doc_index)
-        tf_idf_doc = {word:self.tf_idf_dict[word][doc_index] for word in doc_words}
-        
+        tf_idf_doc = {word: self.tf_idf_dict[word][doc_index] for word in doc_words}
+
         # calculating the numerator
 
         common_words_doc_query = set(doc_words) & set(closest_query)
@@ -71,24 +76,26 @@ class SearchEngine:
             return 0.0
         else:
             return float(numerator) / denominator
-        
+
     def cosine_similarities_docs(self, query):
-        return [(index, self.dp.paths[index], self.cosine_similarity_of_doc(query, index)) for index in range(len(self.dp.paths))]
-    
+        return [(index, self.dp.paths[index], self.cosine_similarity_of_doc(query, index)) for index in
+                range(len(self.dp.paths))]
+
     def cosine_similarity_of_sentence(self, query, doc_index, sentence_index):
         closest_query = [self.dp.get_closest_word_doc(word, doc_index) for word in tokenize(query)]
-        tf_idf_query = {word:self.tf_word_in_one_sentence(word, query)*self.idf_word_in_doc(word, doc_index) for word in closest_query}
+        tf_idf_query = {word: self.tf_word_in_one_sentence(word, query) * self.idf_word_in_doc(word, doc_index) for word
+                        in closest_query}
 
         doc_words = self.dp.document_words(doc_index)
-        tf_idf_sentence = {word:self.tf_idf_sentences_dict[word][sentence_index] for word in doc_words}
-        
+        tf_idf_sentence = {word: self.tf_idf_sentences_dict[word][sentence_index] for word in doc_words}
+
         # calculating the numerator
 
         common_words_doc_query = set(doc_words) & set(closest_query)
         numerator = 0
         for word in common_words_doc_query:
             numerator += tf_idf_query[word] * tf_idf_sentence[word]
-            
+
         # calculating the denumerator
 
         sum1 = sum([tf_idf_query[x] ** 2 for x in list(tf_idf_query.keys())])
@@ -102,8 +109,8 @@ class SearchEngine:
 
     def cosine_similarities_doc_sentences(self, query, doc_index):
         self.calculate_tf_idf_doc(doc_index)
-        return [(index, self.cosine_similarity_of_sentence(query, doc_index, index)) for index in range(len(self.dp.document_sentences(doc_index)))]
-    
+        return [(index, self.cosine_similarity_of_sentence(query, doc_index, index)) for index in
+                range(len(self.dp.document_sentences(doc_index)))]
 
     def get_word_with_highest_tf_in_doc(self, doc_index):
         tf_dict = {}
@@ -116,7 +123,6 @@ class SearchEngine:
 
         highest_tf_word = max(tf_dict, key=tf_dict.get)
         return highest_tf_word
-    
 
     def get_top_five_idf_words_in_doc(self, doc_index):
         idf_dict = {word: self.idf_word_in_all_docs(word) for word in self.dp.document_words(doc_index)}
